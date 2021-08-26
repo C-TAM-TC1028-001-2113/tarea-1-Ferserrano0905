6017bddf16321ab80d634ff23bf5586bfedf271b
Ferserrano0905 <a01411977@itesm.mx>
2021-08-26 14:27:10 -0500

    Actualice el ejercicio 6.

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     # escribe tu código abajo de esta línea
-    pass
+    palabras = int(input("Dame el número de palabras: "))
+
+    paginas = palabras / 475
+    des = (paginas * 60) * .10
+    costo = des * 60
+
+    print("El costo de la publicación es: ", costo)
 
 
 if __name__ == '__main__':
