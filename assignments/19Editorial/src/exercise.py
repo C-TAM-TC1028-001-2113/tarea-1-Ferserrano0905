cc539eeb4e92968c287be9bc599a9c8b3c7bf2ce
Ferserrano0905 <a01411977@itesm.mx>
2021-08-29 16:36:42 -0500

    Actualice el ejercicio 5.

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,10 +1,13 @@
+import math
+
+
 def main():
-    # escribe tu código abajo de esta línea
+    # escribe tu código abajo de esta líne
     palabras = int(input("Dame el número de palabras: "))
 
-    paginas = palabras / 475
+    paginas = math.ceil(palabras / 475)
     des = (paginas * 60) * .10
-    costo = des * 60
+    costo = (paginas * 60) - des
 
     print("El costo de la publicación es: ", costo)
 
